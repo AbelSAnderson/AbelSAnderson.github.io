be2aff3954b305b8c50efe311804f70ef817dbdf
AbelSAnderson <W0722082@myscc.ca>
2021-04-29 21:27:16 -0400

    #1 Started adding Slideshow to Projects section

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,39 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+
+// Slideshow Controls
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Change slides by n
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
+
+    // Set current slide to n
+    function currentSlide(n) {
+    showSlides(slideIndex = n);
+    }
+
+    // Change the slide
+    function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("projectSlide");
+    var dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "flex";  
+    dots[slideIndex-1].className += " active";
+    }
